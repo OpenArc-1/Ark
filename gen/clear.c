22cdd5d42877ce3c3c5b1286ee81ec945e5b57ca
adnan-opnarc <shakirahossain7@gmail.com>
2026-01-23 17:11:09 +0600

    Add constants for screen clearing

--- gen/clear.c
+++ gen/clear.c
@@ -2,6 +2,9 @@
 
 #define VGA_WIDTH 25
 #define VGA_HEIGHT 80
+#define WHITE_ON_BLACK 0x0F
+static volatile uint8_t* const VGA_MEMORY = (volatile uint8_t*)0xB8000;
+
 
 void clear_screen() {
     for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {

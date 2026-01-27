639ff8189e7e6c0067fa625a93ebcce388d3585b
adnan-opnarc <shakirahossain7@gmail.com>
2026-01-27 22:44:00 +0600

    Update ehci.h

--- usb/ehci.h
+++ usb/ehci.h
@@ -1,6 +1,8 @@
 #define EHCI_H
 #ifndef EHCI_H
 
-void ehci_init(); //we need this for now 
+void ehci_init(uint32_t bar0); //we need this for now 
+
+extern struct ehci_controller ehci_ctrlr;
 
 #endif
